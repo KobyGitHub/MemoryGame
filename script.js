6a1167b4ff8feba174b5c41772b3e70820bd19ce
Jim Doyle <jim@divstrong.com>
2022-10-18 16:31:06 -0400

    reworked some code to push this along

--- script.js
+++ script.js
@@ -69,11 +69,12 @@ function createDivsForColors(colorArray) {
     const newDiv = document.createElement("div");
     newDiv.classList.add(color);
     newDiv.setAttribute('id', colorArray.indexOf(color))
-    newDiv.addEventListener("click", flipCard);
+    newDiv.addEventListener("click", handleCardClick); // FlipCard gets called by Handle Card Click
     gameContainer.append(newDiv);
 
     const img = document.createElement('img');
     img.src = "memes/" + colorArray.indexOf(color) + ".jpeg";
+    img.setAttribute('class', 'hide');
     newDiv.append(img)
   }
 }
@@ -84,62 +85,68 @@ function createDivsForColors(colorArray) {
   let cardsWon = []
 
 function handleCardClick(event) {
-    const cards = document.querySelectorAll('img')
-    for( let i = 0; i > cards.length; i++){
-      const optionOneId = cardsChosenId[0]
-      const optionTwoId = cardsChosenId[1]
-      
-      if(optionOneId == optionTwoId) {
-        optionOneId.setAttribute('src', 'Meme.png')
-        optionTwoId.setAttribute('src', 'Meme.png')
-      alert('You have clicked the same image!')
-    }
-    else if (cardsChosen[0] === cardsChosen[1]) {
-      alert('You found a match')
-      cards[optionOneId].setAttribute('src', 'white.png')
-      cards[optionTwoId].setAttribute('src', 'white.png')
-      cards[optionOneId].removeEventListener('click', flipCard)
-      cards[optionTwoId].removeEventListener('click', flipCard)
-      cardsWon.push(cardsChosen)
-    } else {
-      cards[optionOneId].setAttribute('src', 'images/blank.png')
-      cards[optionTwoId].setAttribute('src', 'images/blank.png')
-      alert('Sorry, try again')
-    }
-    cardsChosen = []
-    cardsChosenId = []
-    resultDisplay.textContent = cardsWon.length
-    if  (cardsWon.length === cardArray.length/2) {
-      resultDisplay.textContent = 'Congratulations! You found them all!'
-    }
 
-    }
+    // This is pulling all cards, but you need to know specifically which one was clicked
+    // const cards = document.querySelectorAll('img');
+
+    const card = event.target.src;
+    event.target.classList.remove("hide");
+    console.log(card);
+
+    cardsChosen.push(card);
+    
+    // for( let i = 0; i > cards.length; i++){
+    //   const optionOneId = cardsChosenId[0]
+    //   const optionTwoId = cardsChosenId[1]
+      
+    //   if(optionOneId == optionTwoId) {
+    //     optionOneId.setAttribute('src', 'Meme.png')
+    //     optionTwoId.setAttribute('src', 'Meme.png')
+    //   alert('You have clicked the same image!')
+    // }
+    // else if (cardsChosen[0] === cardsChosen[1]) {
+    //   alert('You found a match')
+    //   cards[optionOneId].setAttribute('src', 'white.png')
+    //   cards[optionTwoId].setAttribute('src', 'white.png')
+    //   cards[optionOneId].removeEventListener('click', flipCard)
+    //   cards[optionTwoId].removeEventListener('click', flipCard)
+    //   cardsWon.push(cardsChosen)
+    // } else {
+    //   cards[optionOneId].setAttribute('src', 'images/blank.png')
+    //   cards[optionTwoId].setAttribute('src', 'images/blank.png')
+    //   alert('Sorry, try again')
+    // }
+    // cardsChosen = []
+    // cardsChosenId = []
+    // resultDisplay.textContent = cardsWon.length
+    // if  (cardsWon.length === cardArray.length/2) {
+    //   resultDisplay.textContent = 'Congratulations! You found them all!'
+    // }
+
+    // }
   }
 
-    function flipCard() {
-      const carders = document.querySelectorAll('img')
-      const cardArray = Array.from(carders)
-      let cardId = this.getAttribute('data-id')
-      
-      const name = Array.from(carders).map(function(name, num, list){
-        if (cardId = num ){
-          return name
-        }
-      })
+  function flipCard() {
+    const carders = document.querySelectorAll('img')
+    const cardArray = Array.from(carders)
+    let cardId = this.getAttribute('data-id')
+    
+    const name = Array.from(carders).map(function(name, num, list){
+      if (cardId = num ){
+        return name
+      }
+    })        
+}
 
-
-   
-    cardsChosen.push(cardArray[cardId])
-    cardsChosenId.push(cardId)
-    this.setAttribute('src', name[cardId].src)
-    if (cardsChosen.length ===2) {
-      setTimeout(handleCardClick, 500)
-    }
-        
-    }
 
    
     
 
+function checkMatch(cards) {
+	if (cards[0] === cards[1]) {
+  	console.log("it's a match");
+  	return true;
+  }
+};
 
 
